019d106d107385b237d388057de11fe44ba31249
NORTH <nordintata0@gmail.com>
2024-05-07 10:23:47 +0100

    function queries the Reddit API to get the subscriber

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,31 +5,33 @@
 import requests
 import sys
 
+
 def number_of_subscribers(subreddit):
-  """
-  This function queries the Reddit API to get the subscriber count for a subreddit.
+    """
+    This function queries the Reddit API to get the subscriber count for a subreddit.
 
-  Args:
-      subreddit: The name of the subreddit to query.
+    Args:
+        subreddit: The name of the subreddit to query.
 
-  Returns:
-      The number of subscribers for the subreddit, or 0 if the subreddit is invalid.
-  """
-  url = f"https://reddit.com/r/{subreddit}/about.json?limit=0"
-  headers = {"User-Agent": "My Cool Custom User Agent"}  # Replace with your own user agent
+    Returns:
+        The number of subscribers for the subreddit, or 0 if the subreddit is invalid.
+    """
+    url = f"https://reddit.com/r/{subreddit}/about.json?limit=0"
+    headers = {"User-Agent": "My Cool Custom User Agent"}
 
-  try:
-    response = requests.get(url, allow_redirects=False, headers=headers)
-    response.raise_for_status()  # Raise an exception for non-200 status codes
+    try:
+        response = requests.get(url, allow_redirects=False, headers=headers)
+        response.raise_for_status()
 
-    data = response.json()
-    return data.get("data", {}).get("subscribers", 0)
-  except (requests.exceptions.RequestException, KeyError):
-    return 0  # Handle request errors and missing data keys
+        data = response.json()
+        return data.get("data", {}).get("subscribers", 0)
+    except (requests.exceptions.RequestException, KeyError):
+        return 0
 
-if __name__ == '__main__':
-  number_of_subscribers = __import__('0-subs').number_of_subscribers
-  if len(sys.argv) < 2:
-    print("Please pass an argument for the subreddit to search.")
-  else:
-    print("{:d}".format(number_of_subscribers(sys.argv[1])))
+
+if __name__ == "__main__":
+    number_of_subscribers = __import__("0-subs").number_of_subscribers
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        print("{:d}".format(number_of_subscribers(sys.argv[1])))
