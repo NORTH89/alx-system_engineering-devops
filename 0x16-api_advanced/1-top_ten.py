3a2a37cdc135795f5900bd12d1c7da134c120906
NORTH <nordintata0@gmail.com>
2024-05-07 11:47:19 +0100

    This function queries the Reddit API

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -3,30 +3,20 @@
 1-main
 """
 import requests
-import sys
 
 
 def top_ten(subreddit):
-    """This function queries the Reddit API to get the titles of the top 10"""
-    url = f"https://reddit.com/r/{subreddit}/hot.json?limit=10"
-    headers = {"User-Agent": "My Cool Custom User Agent"}
-    try:
-        response = requests.get(url, allow_redirects=False, headers=headers)
-        response.raise_for_status()
+    if subreddit is None or type(subreddit) is not str:
+        return 0
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {"User-Agent": "MyCoolReqName/1.0 (by /u/ReplyAdventurous5909)"}
 
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
         data = response.json()
-        posts = data.get("data", {}).get("children", [])
-        for post in posts[:10]:  # Limit to top 10
-            title = post.get("data", {}).get("title")
-            if title:
+        if "data" in data and "children" in data["data"]:
+            for post in data["data"]["children"]:
+                title = post["data"]["title"]
                 print(title)
-    except (requests.exceptions.RequestException, KeyError):
-        print(None)
-
-
-if __name__ == "__main__":
-    number_of_subscribers = __import__("1-top_ten").top_ten
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
     else:
-        top_ten(sys.argv[1])
+        print(None)
