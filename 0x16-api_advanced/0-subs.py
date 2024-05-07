2399b9c85d656711fcad7a69fd946e5176442181
NORTH <nordintata0@gmail.com>
2024-05-07 10:37:38 +0100

    function queries the Reddit API to get the subscriber

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,16 +2,21 @@
 """ get redit subs """
 import requests
 
-
 def number_of_subscribers(subreddit):
-    if subreddit is None or type(subreddit) is not str:
-        return 0
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "MyCoolReqName/1.0 (by /u/ReplyAdventurous5909)"}
-
     response = requests.get(url, headers=headers)
+
     if response.status_code == 200:
         data = response.json()
-        return data["data"]["subscribers"]
+        return data['data']['subscribers']
     else:
         return 0
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        subreddit = sys.argv[1]
+        print("{:d}".format(number_of_subscribers(subreddit)))
